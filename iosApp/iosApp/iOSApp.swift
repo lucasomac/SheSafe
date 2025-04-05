c36c7de1b0a075a315a0778f940d4340af62e0ef
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2025-04-04 22:54:22 -0300

    feat: Integrate Google Sign-In for iOS
    This commit integrates Google Sign-In functionality into the iOS application, allowing users to authenticate with their Google accounts. Key changes include:
    
    -   **AppDelegate Implementation:**
        -   Created an `AppDelegate` class to handle the authentication redirect URL using `GIDSignIn.sharedInstance.handle(url)`.
        -   Implemented the `application(_:open:options:)` method to process incoming URLs, determining if they are handled by Google Sign-In.
    -   **iOSApp Structure:**
        -   Modified `iOSApp` to use the custom `AppDelegate` via `@UIApplicationDelegateAdaptor`.
        -   Added an `onOpenURL` modifier to the main `ContentView`, forwarding any incoming URLs to `GIDSignIn.sharedInstance.handle(url)` for processing.
    -   **Info.plist Configuration:**
        - Added the `GIDServerClientID` and the `GIDClientID`.
        -   Added a custom URL scheme (`com.googleusercontent.apps.908009107434-4cd2v3hd33gi5s910mal0ai7b45bve84`) to the `CFBundleURLTypes`, enabling the app to handle redirects from Google Sign-In.
    - Included the `GoogleSignIn` dependency in the `iosApp` module.

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,10 +1,39 @@
 import SwiftUI
+import shared
+import GoogleSignIn
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+    func application(
+      _ app: UIApplication,
+      open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+    ) -> Bool {
+      var handled: Bool
+
+      handled = GIDSignIn.sharedInstance.handle(url)
+      if handled {
+        return true
+      }
+
+      // Handle other custom URL types.
+
+      // If not handled by this app, return false.
+      return false
+    }
+
+
+}
 
 @main
 struct iOSApp: App {
-	var body: some Scene {
-		WindowGroup {
-			ContentView()
-		}
-	}
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+   var body: some Scene {
+      WindowGroup {
+            ContentView().onOpenURL(perform: { url in
+                GIDSignIn.sharedInstance.handle(url)
+            })
+      }
+   }
 }
+
