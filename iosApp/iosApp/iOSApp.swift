0a8ab347f230af2750fa474c21f95d496de0b7f4
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2025-04-09 19:58:21 -0300

    feat: Integrate Google Sign-In for iOS
    This commit integrates Google Sign-In functionality into the iOS application, allowing users to authenticate with their Google accounts. Key changes include:
    
    -   **AppDelegate Implementation:**
        -   Created an `AppDelegate` class to handle the authentication redirect URL using `GIDSignIn.sharedInstance.handle(url)`.
        -   Implemented the `application(_:open:options:)` method to process incoming URLs, determining if they are handled by Google Sign-In.
    -   **iOSApp Structure:**
        -   Modified `iOSApp` to use the custom `AppDelegate` via `@UIApplicationDelegateAdaptor`.
        -   Added an `onOpenURL` modifier to the main `ContentView`, forwarding any incoming URLs to `GIDSignIn.sharedInstance.handle(url)` for processing.
    -   **Info.plist Configuration:**
        -   Added the `GIDServerClientID` (`712393037708-ju1eifha7h5jhusiqmjlk4lhbmior73r.apps.googleusercontent.com`) and the `GIDClientID` (`712393037708-sl6oh64v4v7aogq139caheuas1gk282d.apps.googleusercontent.com`).
        -   Added a custom URL scheme (`com.googleusercontent.apps.712393037708-ju1eifha7h5jhusiqmjlk4lhbmior73r`) to the `CFBundleURLTypes`, enabling the app to handle redirects from Google Sign-In.

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,6 +1,19 @@
 import SwiftUI
-import shared
 import GoogleSignIn
+
+@main
+struct iOSApp: App {
+
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+	var body: some Scene {
+		WindowGroup {
+            ContentView().onOpenURL { url in
+                GIDSignIn.sharedInstance.handle(url)
+            }
+		}
+	}
+}
 
 class AppDelegate: NSObject, UIApplicationDelegate {
 
@@ -10,6 +23,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     ) -> Bool {
       var handled: Bool
 
+    // Let Google Sign-In handle the URL if it's related to Google Sign-In
       handled = GIDSignIn.sharedInstance.handle(url)
       if handled {
         return true
@@ -20,20 +34,4 @@ class AppDelegate: NSObject, UIApplicationDelegate {
       // If not handled by this app, return false.
       return false
     }
-
-
 }
-
-@main
-struct iOSApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-
-   var body: some Scene {
-      WindowGroup {
-            ContentView().onOpenURL(perform: { url in
-                GIDSignIn.sharedInstance.handle(url)
-            })
-      }
-   }
-}
-
